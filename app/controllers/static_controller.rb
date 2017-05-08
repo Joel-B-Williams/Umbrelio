87c218f53e94de2a827dc20ff9087476feff2aa9
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-05-08 12:17:06 -0500

    add past call to response

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,10 +10,18 @@ class StaticController < ApplicationController
 		key = ENV["DARK_SKY"]
 		latitude = params[:lat]
 		longitude = params[:lng]
-		
-		response = HTTParty.get("#{base_url}/#{key}/#{latitude},#{longitude}")
-		p "*"*50
+		api_url = "#{base_url}/#{key}/#{latitude},#{longitude}"
+		current = HTTParty.get(api_url)
+		past = HTTParty.get(api_url+",2017-05-08T12:00:00")
+		p"*"*80 
+		response = current.parsed_response
+		past = past.parsed_response
+		response["past"] = past
 		p response
+		p"*"*80 
+		
+		p response["past"]["daily"]#.daily.data.temperatureMax
+		# response = current
 		respond_to do |format|
 			format.json { render json: response }
 			# format.html { 
