dd25e59b7c036b43eabea2f017e545cc8563845b
Joel Williams <joel.b.williams.is@gmail.com>
2017-06-24 14:04:24 -0500

    spacing

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -14,7 +14,6 @@ class ForecastsController < ApplicationController
 		@forecast.user_id = current_user.id
 
 		if @forecast.save
-			
 			response = @forecast.get_current_forecast(latitude, longitude)
 
 			@forecast.build_full_response(@forecast.assemble_past_forecasts(latitude, longitude), response)
@@ -34,4 +33,4 @@ class ForecastsController < ApplicationController
 		def forecast_params
 			params.permit(:lat, :lng)
 		end
-end
+	end
